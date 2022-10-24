5549bf71bb371a0d49ed9cbfbb794859ea6e647c
AleksandraPauk <aleksandra.pauk@ukr.net>
2022-10-24 18:26:09 +0300

    add first part of HW

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,57 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryRef = document.querySelector(".gallery")
+const cardsMarkup = makeGallery(galleryItems)
+
+galleryRef.addEventListener("click", showModal)
+galleryRef.insertAdjacentHTML("beforeend", cardsMarkup)
+
+function makeGallery (items){
+    return items.map(item => {
+        return `
+        <div class="gallery__item">
+            <a class="gallery__link" href="${item.original}">
+                <img
+                class="gallery__image"
+                src="${item.preview}"
+                data-source="${item.original}"
+                alt="${item.description}"
+                />
+            </a>
+        </div>`
+    })
+    .join("")
+}
+    
+function showModal(event) {
+    if (event.target.nodeName !== "IMG") {
+        return
+    }
+
+    event.preventDefault()
+
+    const instance = basicLightbox.create(`
+    <div class="modal">
+        <img src="${event.target.dataset.source}">
+    </div>
+    `)
+    instance.show()
+
+    window.addEventListener("keydown", closeModal)
+    
+    function closeModal(event) {
+        if (event.code === "Escape") {
+            instance.close()
+            window.removeEventListener("keypress", closeModal)
+        }
+    }
+}
+
+
+
+    
+    
+ 
+
+
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,34 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryRef = document.querySelector(".gallery")
+const cardMarkup = addGalleryItems(galleryItems)
+
+galleryRef.insertAdjacentHTML("beforeend", cardMarkup)
+galleryRef.addEventListener("click", showModal)
+
+function addGalleryItems(items) {
+    return items.map((item) => {
+        return `
+        <a class="gallery__item" href="${item.original}">
+            <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
+        </a>`
+    })
+        .join("")
+}
+
+function showModal(event) {
+    console.log(event.target)
+    event.preventDefault()
+
+    if (event.target.nodeName !== "IMG") {
+            return
+    }
+    
+    let gallery = new SimpleLightbox('.gallery a', {
+        captionData: "alt",
+        captionDelay: 250,
+    })
+
+}
+
